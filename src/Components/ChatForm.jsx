ac4a916430653a2378d5a413d7173a25d9ea10fc
Kathy Bauer <kbauer17@my.wctc.edu>
2024-02-28 09:45:14 -0600

    [1.18] Disable name, set focus message upon submit

--- src/Components/ChatForm.jsx
+++ src/Components/ChatForm.jsx
@@ -1,35 +1,43 @@
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
 
 const ChatForm = (props) => {
-    const { onSend } = props;
-    const [ name, setName] = useState('');
-    const [ message, setMessage] = useState('');
+  const { onSend } = props;
+  const [ name, setName ] = useState('');
+  const [ message, setMessage ] = useState('');
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        onSend({ name, message });
-      }
-    
-    return (
+  const nameEl = useRef(null);
+  const messageEl = useRef(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setMessage('');
+    nameEl.current.disabled = true;
+    messageEl.current.focus();
+    onSend({ name, message });
+  }
+  
+  return (
     <form onSubmit={handleSubmit} className='Chat-form'>
       <input 
+        ref={nameEl}
         type="text" 
-        id="name"
+        id="name" 
         onChange={ (e) => setName(e.target.value) }
         name="name"
-        value={name} 
+        value={name}
         placeholder="Name"
         autoFocus
         autoComplete="off"
         autoCapitalize="off">
       </input><br />
       <input
+        ref={messageEl}
         disabled={ name.trim().length === 0 }
         type="text" 
-        id="Message"
+        id="Message" 
         onChange={ (e) => setMessage(e.target.value) }
         name="message"
-        value={message} 
+        value={message}
         placeholder="Message"
         autoComplete="off"
         autoCapitalize="off">
