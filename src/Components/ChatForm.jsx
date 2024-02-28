c023e0255e5380bcf7c4cf516311cc1f23e7e04f
Kathy Bauer <kbauer17@my.wctc.edu>
2024-02-28 08:56:58 -0600

    [1.05] Form elements & css - Adjust viewport meta tag to prevent iphone zoom on input focus

--- src/Components/ChatForm.jsx
+++ src/Components/ChatForm.jsx
@@ -2,9 +2,26 @@ import React from 'react';
 
 const ChatForm = () => {
   return (
-    <React.Fragment>
-      Hello Chat Form!
-    </React.Fragment>
+    <form className='Chat-form'>
+      <input 
+        type="text" 
+        id="name" 
+        placeholder="Name"
+        autoFocus
+        autoComplete="off"
+        autoCapitalize="off">
+      </input><br />
+      <input
+        type="text" 
+        id="Message" 
+        placeholder="Message"
+        autoComplete="off"
+        autoCapitalize="off">
+      </input><br />
+      <button type="submit">
+        Send Message
+      </button>
+    </form>
   );
 }
 
